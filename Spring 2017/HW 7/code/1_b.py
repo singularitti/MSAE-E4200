9e78b206d9a01417c099a50d6a32c572fbabf87a
Qi Zhang <singularitti@outlook.com>
2019-04-06 19:16:02 -0400

    Add HW 7 for Spring 2017

---
+++ Spring 2017/HW 7/code/1_b.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# created at 9 avr. 2017 17:49
+# created by Nil-Zil
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.linalg import eigvals
+
+gamma_1 = 1
+gamma_2 = 2
+a = 1
+
+
+def dynamical_matrix(k):
+    return np.matrix(
+        [[2 * (gamma_1 + gamma_2), -2 * gamma_2 * np.exp(-k * a * 1j) - 2 * gamma_1],
+         [-2 * gamma_2 * np.exp(k * a * 1j) - 2 * gamma_1, 2 * (gamma_1 + gamma_2)]]
+    )
+
+
+def evals(k):
+    return np.real(np.sort(eigvals(dynamical_matrix(k))))
+
+
+k = np.linspace(-2*np.pi / a, 2*np.pi / a, num=500)
+plt.plot(k, [evals(kk) for kk in k])
+plt.show()
